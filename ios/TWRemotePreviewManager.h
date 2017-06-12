9eb9cad4499e6997d68a89d2e91509339c0e656e
devfiction <mitrendov@gmail.com>
2017-06-12 16:39:40 +0400

    react-native-twilio-video-webrtc
    react-native app with twilio framework.

---
+++ ios/TWRemotePreviewManager.h
@@ -0,0 +1,6 @@
+#import <React/RCTViewManager.h>
+
+@interface TWRemotePreviewManager : RCTViewManager
+
+
+@end
---
+++ ios/TWVideoPreviewManager.h
@@ -0,0 +1,8 @@
+#import <React/UIView+React.h>
+
+#import <React/RCTViewManager.h>
+
+@interface TWVideoPreviewManager : RCTViewManager
+
+
+@end
